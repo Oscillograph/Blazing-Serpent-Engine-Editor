9e73682447282122fed798b23c71d5a19c4c48e4
Living Broscillograph <totservis@gmail.com>
2023-08-13 20:11:18 +0300

    Viewport and stuff

--- main.cpp
+++ main.cpp
@@ -5,61 +5,71 @@
 #include "./src/SceneViewer.h"
 
 class EditorGuiLayer : public BSE::ImGuiLayer {
+public:
+	EditorGuiLayer() {
+		broTexture = BSE::Texture2D::Create("./assets/img/broscillograph.png");
+		broTexture->Bind();
+	}
+	
+	~EditorGuiLayer() {
+		if (broTexture != nullptr)
+			delete broTexture;
+	}
+	
 	void OnImGuiRender(float time) {
-		layerTime += time;
-		if (layerTime >= BSE_FPS60){
-			FixImGuiContext(m_ImGuiContext);
-			
-			// If you strip some features of, this demo is pretty much equivalent to calling DockSpaceOverViewport()!
-			// In most cases you should be able to just call DockSpaceOverViewport() and ignore all the code below!
-			// In this specific demo, we are not using DockSpaceOverViewport() because:
-			// - we allow the host window to be floating/moveable instead of filling the viewport (when opt_fullscreen == false)
-			// - we allow the host window to have padding (when opt_padding == true)
-			// - we have a local menu bar in the host window (vs. you could use BeginMainMenuBar() + DockSpaceOverViewport() in your code!)
-			// TL;DR; this demo is more complicated than what you would normally use.
-			// If we removed all the options we are showcasing, this demo would become:
-			//     void ShowExampleAppDockSpace()
-			//     {
-			//         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-			//     }
-			
-			static bool dockspace_open = true;
-			static bool opt_fullscreen = true;
-			static bool opt_padding = false;
-			static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-			
-			// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-			// because it would be confusing to have two docking targets within each others.
-			ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-			if (opt_fullscreen)
-			{
-				const ImGuiViewport* viewport = ImGui::GetMainViewport();
-				ImGui::SetNextWindowPos(viewport->WorkPos);
-				ImGui::SetNextWindowSize(viewport->WorkSize);
-				ImGui::SetNextWindowViewport(viewport->ID);
-				ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-				ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-				window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-				window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-			}
-			else
-			{
-				dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
-			}
-			
-			// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
-			// and handle the pass-thru hole, so we ask Begin() to not render a background.
-			if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-				window_flags |= ImGuiWindowFlags_NoBackground;
-			
-			// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-			// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-			// all active windows docked into it will lose their parent and become undocked.
-			// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-			// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-			if (!opt_padding)
-				ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-			ImGui::Begin("DockSpace Demo", &dockspace_open, window_flags);
+		FixImGuiContext(m_ImGuiContext);
+
+		// If you strip some features of, this demo is pretty much equivalent to calling DockSpaceOverViewport()!
+		// In most cases you should be able to just call DockSpaceOverViewport() and ignore all the code below!
+		// In this specific demo, we are not using DockSpaceOverViewport() because:
+		// - we allow the host window to be floating/moveable instead of filling the viewport (when opt_fullscreen == false)
+		// - we allow the host window to have padding (when opt_padding == true)
+		// - we have a local menu bar in the host window (vs. you could use BeginMainMenuBar() + DockSpaceOverViewport() in your code!)
+		// TL;DR; this demo is more complicated than what you would normally use.
+		// If we removed all the options we are showcasing, this demo would become:
+		//     void ShowExampleAppDockSpace()
+		//     {
+		//         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		//     }
+		
+		static bool dockspace_open = true;
+		static bool opt_fullscreen = true;
+		static bool opt_padding = false;
+		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+		
+		// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+		// because it would be confusing to have two docking targets within each others.
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+		if (opt_fullscreen)
+		{
+			const ImGuiViewport* viewport = ImGui::GetMainViewport();
+			ImGui::SetNextWindowPos(viewport->WorkPos);
+			ImGui::SetNextWindowSize(viewport->WorkSize);
+			ImGui::SetNextWindowViewport(viewport->ID);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+		}
+		else
+		{
+			dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+		}
+		
+		// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+		// and handle the pass-thru hole, so we ask Begin() to not render a background.
+		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+			window_flags |= ImGuiWindowFlags_NoBackground;
+		
+		// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+		// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+		// all active windows docked into it will lose their parent and become undocked.
+		// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+		// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+		if (!opt_padding)
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		
+		ImGui::Begin("DockSpace Demo", &dockspace_open, window_flags);
 			if (!opt_padding)
 				ImGui::PopStyleVar();
 			
@@ -67,8 +77,8 @@ class EditorGuiLayer : public BSE::ImGuiLayer {
 				ImGui::PopStyleVar(2);
 			
 			// Submit the DockSpace
-			ImGuiIO& io = ImGui::GetIO();
-			if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+			// ImGuiIO& io = ImGui::GetIO();
+			if (m_io->ConfigFlags & ImGuiConfigFlags_DockingEnable)
 			{
 				ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
 				ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
@@ -77,68 +87,46 @@ class EditorGuiLayer : public BSE::ImGuiLayer {
 			if (ImGui::BeginMenuBar())
 			{
 				if (ImGui::BeginMenu(u8"Файл"))
-				{
-					// Disabling fullscreen would allow the window to be moved to the front of other windows,
-					// which we can't undo at the moment without finer window depth/z control.
-					ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
-					ImGui::MenuItem("Padding", NULL, &opt_padding);
-					ImGui::Separator();
-					
-					if (ImGui::MenuItem("Flag: NoSplit",                "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0))                 { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
-					if (ImGui::MenuItem("Flag: NoResize",               "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0))                { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
-					if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0))  { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
-					if (ImGui::MenuItem("Flag: AutoHideTabBar",         "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0))          { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
-					if (ImGui::MenuItem("Flag: PassthruCentralNode",    "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
-					ImGui::Separator();
-					
-					if (ImGui::MenuItem("Close", NULL, false))
-						dockspace_open = false;
+				{						
+					if (ImGui::MenuItem(u8"Выход"))
+						BSE::Application::Get()->Close();
 					ImGui::EndMenu();
 				}
 				ImGui::EndMenuBar();
 			}
-			
-			static char buf[256] = u8"Фыва";
-			static char textinput[256] = u8"Wonder what это такое?";
-			static float f = 0.0f;
-			
-			ImGui::Begin(u8"Начало");
-			// TODO: AssetManager, GameData storage
-			// ImGui::Image((void*)(BSE::AssetManager->GetTexture(GameData->Textures[0])->GetID()), {1.0f, 1.0f});
-			ImGui::Text(u8"Превед, мир!!!!! %d", 111);
-			// ImGui::ColorPicker4("Color that Square", glm::value_ptr(m_SquareColor))
-			ImGui::Button(u8"Сохранить");
-			// Important: inputs with the same label are considered by ImGui as kind of the same object
-			ImGui::InputText(u8"Строка", buf, IM_ARRAYSIZE(buf));
-			ImGui::Image(
-				(void*)BSE::GameData::m_FrameBuffer->GetColorAttachmentID(), 
-				ImVec2(600.0f, 400.0f),
-				{0, 1},
-				{1, 0}
-				);
-			ImGui::InputText(u8"Строка Другая", textinput, IM_ARRAYSIZE(textinput));
-			ImGui::SliderFloat(u8"Внезапная дробь", &f, 0.0f, 1.0f);
-			ImGui::Text(u8"Дробь равна: %f", f);
-			
-			/*
-			//static char profileResults[200] = "";
-			auto profileResults = BSE::Profiler::Read();
-			for (auto kv : profileResults){
-				auto name = kv.first;
-				float time = kv.second;
-				ImGui::Text((name + u8"  %.3f ms").c_str(), time);
-			}
-			*/
+		
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+			ImGui::Begin(u8"Сцена");
+				// TODO: AssetManager, GameData storage
+				// ImGui::Image((void*)(BSE::AssetManager->GetTexture(GameData->Textures[0])->GetID()), {1.0f, 1.0f});
+				ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
+				if ((m_ViewportSize.x != viewportPanelSize.x) || (m_ViewportSize.y != viewportPanelSize.y)){
+					m_ViewportSize = {viewportPanelSize.x, viewportPanelSize.y};
+					BSE::GameData::m_FrameBuffer->Resize(m_ViewportSize);
+					BSE::GameData::m_CameraController->OnResize(viewportPanelSize.x, viewportPanelSize.y);
+				}
+				// BSE_INFO("Viewport size is: {0}, {1}", viewportPanelSize.x, viewportPanelSize.y);
+				uint32_t sceneTexture = BSE::GameData::m_FrameBuffer->GetColorAttachmentID();
+				ImGui::Image(
+					(void*)sceneTexture, 
+					{m_ViewportSize.x, m_ViewportSize.y},
+					{0, 1},
+					{1, 0}
+					);
 			ImGui::End();
-			
+			ImGui::PopStyleVar();
+		
+			ImGui::Begin(u8"Разные настройки");
+				ImGui::ColorPicker4(u8"Цвет квадратов", glm::value_ptr(BSE::GameData::CustomColor));
 			ImGui::End();
-			
-			layerTime = 0.0f;
-		}
+		
+		ImGui::End();
 	}
 	
 private:
 	float layerTime = 0.0f;
+	glm::vec2 m_ViewportSize = {600.0f, 400.0f};
+	BSE::Texture2D* broTexture;
 };
 
 class Editor : public BSE::Application {
@@ -153,8 +141,10 @@ public:
 		m_ImGuiLayerEnabled = true;
 		m_WinTitle = "BSE Editor";
 		m_Window->SetTitle(m_WinTitle);
-		// m_Window->SetVSync(false);
-		m_Window->SetVSync(true);
+		m_Window->SetVSync(false);
+		// m_Window->SetVSync(true);
+		
+		BSE::GameData::FPS_deltaTime = BSE_FPS60;
 	}
 	
 	~Editor(){
--- src/SceneViewer.h
+++ src/SceneViewer.h
@@ -29,12 +29,13 @@ public:
 		float uw = (float)w/(float)h;
 		// float uh = 2*(float)h/(float)h;
 		m_CameraController = new BSE::OrthographicCameraController(uw, 1.5f, true);
+		BSE::GameData::m_CameraController = m_CameraController;
 		// ------------------------------------------------
 		BSE::FrameBufferSpecification fbSpec;
 		fbSpec.Width = m_Window->GetWidth();
 		fbSpec.Height = m_Window->GetHeight();
 		m_FrameBufferA = BSE::FrameBuffer::Create(fbSpec);
-		m_FrameBufferB = BSE::FrameBuffer::Create(fbSpec);
+		// m_FrameBufferB = BSE::FrameBuffer::Create(fbSpec);
 		BSE::GameData::m_FrameBuffer = m_FrameBufferA;
 		
 	}
@@ -45,79 +46,77 @@ public:
 	
 	int OnUpdate(float time) override {
 		BSE_PROFILER(u8"Sandbox2D Layer OnUpdate");
-		layerTime += time;
+
 		static int frame = 0;
 		// BSE_TRACE("Layer time counter increased");
 		
 		// --------------------------------------------------
 		// RENDER
 		if (BSE::RenderCommand::GetAPI() != nullptr){
-			if (layerTime >= BSE_FPS60){
-				BSE_PROFILE_SCOPE("Sandbox2D OnUpdate");
-				
-				// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-				// CAMERA
-				m_CameraController->OnUpdate(layerTime);
-				// BSE_TRACE("Camera controller updated");
-				// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-				
-				// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-				// Show some info about FPS on WindowTitle
-				int fps = round(1 / layerTime);
-				if (frame == fps){
-					frame = 0;
-				}
-				char buf[5];
-				sprintf(buf, "%d", fps);
-				m_Window->SetTitle(m_Title + " :: FPS = " + buf);
-				// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-				// Bind a buffer to work with
-				{
-					BSE_PROFILE_SCOPE(u8"Framebuffer Bind");
-					if (BSE::GameData::m_FrameBuffer == m_FrameBufferA) {
-						m_FrameBufferB->Bind();
-					} else {
-						m_FrameBufferA->Bind();
-					}
-				}
-				
-				// BSE_TRACE("Window Title changed");
-				BSE::Renderer2D::Clear({0.2f, 0.2f, 0.4f, 1});
-				BSE::Renderer2D::BeginScene(m_CameraController->GetCamera());
-				
-				for (int x = 0; x < 100; x++){
-					for (int y = 0; y < 100; y++){
-						BSE::Renderer2D::DrawFilledRect(
-							{-1.0f + x*0.06f, -1.0f + y*0.06f}, 
-							{0.05f, 0.05f},
-							// 0.0f + frame * (180 / fps),
-							{0.2f, 0.7f, 0.3f, 1.0f}
-							);
-					}
-					// BSE_INFO("x = {0}, y = {1}", x , y);
-				}
-				
-				BSE::Renderer2D::EndScene();
-				
-				// Unbind current buffer
-				{
-					BSE_PROFILE_SCOPE(u8"Framebuffer Unbind");
-					if (BSE::GameData::m_FrameBuffer == m_FrameBufferA) {
-						m_FrameBufferB->Unbind();
-					} else {
-						m_FrameBufferA->Unbind();
-					}
-				}
-				
-				// swap buffers
-				BSE::GameData::m_FrameBuffer = (BSE::GameData::m_FrameBuffer == m_FrameBufferA) ? m_FrameBufferB : m_FrameBufferA;
-				
-				layerTime = 0.0f;
-				frame++;
-				return 1; // 1 - changed something
-			} else {
-				return 0; // 0 - no changes on screen
+			BSE_PROFILE_SCOPE("Sandbox2D OnUpdate");
+			
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			// CAMERA
+			m_CameraController->OnUpdate(time);
+			// BSE_TRACE("Camera controller updated");
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			// Show some info about FPS on WindowTitle
+			int fps = round(1 / time);
+			if (frame == fps){
+				frame = 0;
 			}
+			char buf[5];
+			sprintf(buf, "%d", fps);
+			m_Window->SetTitle(m_Title + " :: FPS = " + buf);
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			// Bind a buffer to work with
+			{
+				BSE_PROFILE_SCOPE(u8"Framebuffer Bind");
+				//if (BSE::GameData::m_FrameBuffer == m_FrameBufferA) {
+				//	m_FrameBufferB->Bind();
+				//} else {
+					m_FrameBufferA->Bind();
+				//}
+			}
+			
+			// BSE_TRACE("Window Title changed");
+			BSE::Renderer2D::Clear({0.2f, 0.2f, 0.4f, 1});
+			BSE::Renderer2D::BeginScene(m_CameraController->GetCamera());
+			
+			for (int x = 0; x < 100; x++){
+				for (int y = 0; y < 100; y++){
+					BSE::Renderer2D::DrawFilledRect(
+						{-1.0f + x*0.06f, -1.0f + y*0.06f}, 
+						{0.05f, 0.05f},
+						// 0.0f + frame * (180 / fps),
+						// {0.2f, 0.7f, 0.3f, 1.0f}
+						BSE::GameData::CustomColor
+						);
+				}
+				// BSE_INFO("x = {0}, y = {1}", x , y);
+			}
+			
+			BSE::Renderer2D::EndScene();
+			
+			// Unbind current buffer
+			{
+				BSE_PROFILE_SCOPE(u8"Framebuffer Unbind");
+				//if (BSE::GameData::m_FrameBuffer == m_FrameBufferA) {
+				//	m_FrameBufferB->Unbind();
+				//} else {
+					m_FrameBufferA->Unbind();
+				//}
+			}
+			
+			// swap buffers
+			// BSE::GameData::m_FrameBuffer = (BSE::GameData::m_FrameBuffer == m_FrameBufferA) ? m_FrameBufferB : m_FrameBufferA;
+			
+			frame++;
+			return 1; // 1 - changed something
+		} else {
+			return 0; // 0 - no changes on screen
 		}
 		return 0; // RendererAPI == nullptr
 	}
@@ -134,7 +133,7 @@ private:
 	BSE::OrthographicCameraController* m_CameraController = nullptr;
 	
 	BSE::FrameBuffer* m_FrameBufferA;
-	BSE::FrameBuffer* m_FrameBufferB;
+	// BSE::FrameBuffer* m_FrameBufferB;
 };
 
 #endif
