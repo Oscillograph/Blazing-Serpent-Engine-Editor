af25ad9719a26d7193394b2f4ad8d4603852dcd4
Living Broscillograph <totservis@gmail.com>
2023-08-20 11:37:33 +0300

    ClientData for better interlayer communication

---
+++ src/ClientData.h
@@ -0,0 +1,12 @@
+#include <BSE_Client.h>
+
+struct ClientData {
+	static bool ViewPortFocused;
+	
+	
+	// Scene Viewer
+	static BSE::Scene* m_ActiveScene;
+	static BSE::Entity* m_Square;
+	static BSE::Entity* m_CameraA;
+	static BSE::Entity* m_CameraB;
+};
